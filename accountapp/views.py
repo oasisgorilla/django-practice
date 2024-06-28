7ae3bdec3ef5f258db11921bf443e8f1dda776a6
YongJe <yongjae0522@gmail.com>
2024-06-28 21:10:50 +0900

    lecture 8 end

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request): # view 만들기
-    return HttpResponse('Hello World!')
+    return render(request, 'base.html')
 
