883af32d9f70c6a5775298bc0c8a0fcc8dc6098d
oasisgorilla <yongjae0522@gmail.com>
2024-06-28 17:08:30 +0900

    first commit

---
+++ accountapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hello_world(request): # view 만들기
+    return HttpResponse('Hello World!')
+
